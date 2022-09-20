ed0d6e847e359da37e44db0271e23c10ffc6fc89
Dimastaqbirt <dimastrr01@gmail.com>
2022-09-20 15:13:02 +0700

    membuat tampil.php di view. membuat tampil.php di controler dan mengubah file config ->routers

---
+++ application/views/tampil.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latian Pertama</title>
+</head>
+<body>
+    <p>Latihan Pertama saya belajar framework Codeignniter</p>
+    <p>framework Codeignniter sangat mudah dipahami</p>
+</body>
+</html>
