ae4b6b721b6433b54ed53d7655c1c7fcee4b6f3c
Dimastaqbirt <dimastrr01@gmail.com>
2022-09-20 15:58:36 +0700

    membuat tampil.php di view. membuat tampil.php di controler dan mengubah file config ->routers

--- application/views/tampil.php
+++ application/views/tampil.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latian Pertama</title>
+    <link rel="stylesheet" href="<?= base_url('assets/css/boostrap.min.css')?>">
 </head>
 <body>
     <p>Latihan Pertama saya belajar framework Codeignniter</p>
     <p>framework Codeignniter sangat mudah dipahami</p>
 </body>
+<script src="<?= base_url('assets/js/boostrap.bundle.min.js')?>"></script>
 </html>
