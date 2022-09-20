4de674f0b6371768f5f3837bcac7ea021b74b2e7
Dimastaqbirt <dimastrr01@gmail.com>
2022-09-20 16:50:45 +0700

    coba koneksi boostrap

--- application/views/tampil.php
+++ application/views/tampil.php
@@ -5,11 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latian Pertama</title>
-    <link rel="stylesheet" href="<?= base_url('assets/css/boostrap.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>">
 </head>
 <body>
-    <p>Latihan Pertama saya belajar framework Codeignniter</p>
-    <p>framework Codeignniter sangat mudah dipahami</p>
+    <div class="vh-100 d-flex justify-content-center align-items-center bg-warning">
+        <div class="card h-50 p-3 shadow text-center">
+            <p>Latihan Pertama saya belajar framework Codeignniter</p>
+            <p>framework Codeignniter sangat mudah dipahami</p>
+        </div>
+    </div>
 </body>
-<script src="<?= base_url('assets/js/boostrap.bundle.min.js')?>"></script>
+<script src="<?= base_url('assets/js/bootstrap.bundle.min.js')?>"></script>
+<?php  
+var_dump (base_url('assets/js/boostrap.bundle.min'));
+;?>
 </html>
